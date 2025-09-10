ad3f55b99de027c47c3b7d5b25c6f155ac114544
amruta <raskondaamruta32@gamil.com>
2025-09-10 11:53:19 +0530

    commit

---
+++ kochi/src/test/java/demo/Sample.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello kochi students");
+		System.out.println("hello kochi students");
+		System.out.println("hello kochi students");
+		System.out.println("hello kochi students");
+		System.out.println("hello kochi students");
+	}
+
+}
